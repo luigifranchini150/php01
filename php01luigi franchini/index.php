e372f58135e87185f2d45fececab2dcf5b091e0c
luigifranchini150 <luigifranchini150@gmail.com>
2025-09-30 10:04:34 +0200

    Add files via upload

---
+++ php01luigi franchini/index.php
@@ -0,0 +1,18 @@
+<?php
+
+const INTERO = 24;
+const VIRGOLA = 2.4;
+const STRINGA = "stringa";
+const BOOLEANO = true;
+
+var_dump(INTERO);
+var_dump(VIRGOLA);
+var_dump(STRINGA);
+var_dump(BOOLEANO);
+
+
+
+
+
+
+
