db71cd4e6551dea81189fbd648aac76237f83dc9
luigifranchini150 <luigifranchini150@gmail.com>
2025-09-30 11:14:49 +0200

    Add files via upload

---
+++ php02luigi franchini/index.php
@@ -0,0 +1,17 @@
+ <?php 
+
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perche";
+$text6 = "$text2";
+$text7 = "bevuto";
+$text8 = "tutto";
+
+
+$textConcatenata = $text1 . " " . $text2 . " " . $text3 . $text4 . " " . $text5 . " " . $text6 . " " . $text7 . " " . $text8 ."\n";
+
+
+echo $textConcatenata;
+
